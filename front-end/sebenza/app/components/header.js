c053ed727b65b0d903e8430eb28fce2e26035c4e
Sihle <sshabangu061@gmail.com>
2024-09-28 14:59:13 +0200

    final pushes

--- front-end/sebenza/app/components/header.js
+++ front-end/sebenza/app/components/header.js
@@ -5,7 +5,7 @@ import { GrSearch } from "react-icons/gr";
 export default function Header() {
   return (
     <div className='h-16 w-screen flex flex-row justify-between items-center p-10 '>
-        <h2 className='font-black text-3xl'>LOGO</h2>
+        <h2 className='font-black text-3xl'>SEBENZA</h2>
         <ul className='flex flex-row w-1/2 justify-between '>
             <li className='cursor-pointer hover:text-teal-500 manrope font-semibold'><Link href={"/"}>Home</Link></li>
             <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'><Link href={"/jobs"}>Jobs</Link></li>
