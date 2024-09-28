11ef263779cc184056a6f6a9fc266a49ca9a1b66
Sihle <sshabangu061@gmail.com>
2024-09-28 12:59:08 +0200

    able to connect to the jobs page

--- front-end/sebenza/app/components/header.js
+++ front-end/sebenza/app/components/header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <h2 className='font-black text-3xl'>LOGO</h2>
         <ul className='flex flex-row w-1/2 justify-between '>
             <li className='cursor-pointer hover:text-teal-500 manrope font-semibold'><Link href={"/"}>Home</Link></li>
-            <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'>Jobs</li>
+            <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'><Link href={"/jobs"}>Jobs</Link></li>
             <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'>Explore</li>
             <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'><Link href={"/mentors"}>Mentors</Link></li>
             <li className='cursor-pointer  hover:text-teal-500 manrope font-semibold'>Career Guidance</li>
